9c60337180e5dc5d26ba4b91253a0dddc35aaabe
EliArq <Eliana11_1999@hotmail.com>
2025-07-20 17:08:30 +0200

    Update Tridiagonal solve.py

--- computational/Tridiagonal solve.py
+++ computational/Tridiagonal solve.py
@@ -5,8 +5,9 @@ import numpy as np
 import time
 import matplotlib.pyplot as plt
 
-inicio = time.time()
+
 def TridiagonalSolver(d,o,u,r):
+    inicio = time.time()
     ro = np.zeros(len(r))
     n = len(d)
     ro[0] = r[0]/d[0]
@@ -27,10 +28,9 @@ def TridiagonalSolver(d,o,u,r):
     
     for i in range(n-2,-1,-1):
         soluciones[i] = ro[i] - (h[i]*soluciones[i+1])
-
-    termino = time.time()    
-    tiempo = termino-inicio 
-    return soluciones, tiempo
+    termino = time.time()
+    return soluciones, termino,inicio
+    
 
 #la diagonal debe ser mayor a la suma de u y o para cada i
 matriz = [[3,1,0,0],[1,5,2,0],[0,2,7,3],[0,0,3,9]]
@@ -40,6 +40,8 @@ u = np.diag(matriz,-1)
 o = np.diag(matriz,1)
 r = np.array([1,2,3,4])
 
+inicio,termino = TridiagonalSolver(d,o,u,r)[1:]    
+tiempo = termino-inicio 
 
 #print(TridiagonalSolver(d,o,u,r))
 
@@ -49,20 +51,22 @@ longitud_matrix = []
 tiempo_ejec = []
 
 for i in range(1,10,3):
-    matriz2 = np.random.rand(1000*(i),1000*(i))
-    d = np.diag(matriz2)
-    u = np.diag(matriz2,-1)
-    o = np.diag(matriz2,1)
-    r = np.random.rand(1000*(i),1)
+    n = 1000*i
+    d = np.random.rand(n)
+    u = np.random.rand(n-1)
+    o = np.random.rand(n-1)
+    matriz2 = np.diag(d) + np.diag(u,1) + np.diag(o,-1)
+    r = np.random.rand(1000*(i))
 
-    solucion2, tiempo = TridiagonalSolver(d,o,u,r)
+    solucion2,termino,inicio = TridiagonalSolver(d,o,u,r)
+    tiempo = termino-inicio
     tiempo_ejec.append(tiempo)
     longitud_matrix.append(len(matriz2))
 
 
-plt.plot(tiempo_ejec,longitud_matrix, 'ro')
-plt.xlabel('tiempo de ejectucion (s)')
-plt.ylabel('longitud de la matriz (len)')
+plt.plot(longitud_matrix,tiempo_ejec, 'ro')
+plt.ylabel('tiempo de ejecución (s)')
+plt.xlabel('longitud de la matriz (len)')
 plt.title('Representación del tiempo de ejecución respecto del tamaño de la matriz')
 plt.show()
 
@@ -74,8 +78,12 @@ longitud_matrix3 = []
 tiempo_ejec3 = []
 
 for m in range(1,10,3):
-    matriz3 = np.random.rand(1000*(m),1000*(m))
-    r = np.random.rand(1000*(m),1)
+    n = 1000*m
+    d = np.random.rand(n)
+    u = np.random.rand(n-1)
+    o = np.random.rand(n-1)
+    matriz3 = np.diag(d) + np.diag(u,1) + np.diag(o,-1)
+    r = np.random.rand(1000*(m))
     inicio = time.time()
 
     matriz_x = np.linalg.solve(matriz3,r)
@@ -83,23 +91,25 @@ for m in range(1,10,3):
     tiempo = termino-inicio 
     tiempo_ejec2.append(tiempo)
     longitud_matrix2.append(len(matriz_x))
- 
     
 for j in range(1,10,3):
-    matriz3 = np.random.rand(1000*(j),1000*(j))
-    r = np.random.rand(1000*(j),1)
+    n = 1000*j
+    d = np.random.rand(n)
+    u = np.random.rand(n-1)
+    o = np.random.rand(n-1)
+    matriz3 = np.diag(d) + np.diag(u,1) + np.diag(o,-1)
+    r = np.random.rand(1000*(j))
     inicio = time.time()
 
     matriz3_inv = np.linalg.inv(matriz3)
 
-    sol1 = matriz3_inv*r
+    sol1 = matriz3_inv@r
     termino = time.time()    
     tiempo = termino-inicio 
 
     tiempo_ejec3.append(tiempo)
     longitud_matrix3.append(len(matriz3))
 
- 
 
 plt.plot(tiempo_ejec, longitud_matrix, 'ro', 
          tiempo_ejec2, longitud_matrix2, 'yo', 
@@ -107,8 +117,8 @@ plt.plot(tiempo_ejec, longitud_matrix, 'ro',
 
 
 plt.legend(labels=['tridiagonal', 'linalg', 'inversa'], loc = 'best')
-plt.xlabel('tiempo de ejectucion (s)')
+plt.xlabel('tiempo de ejecución (s)')
 plt.ylabel('longitud de la matriz (len)')
 plt.title('Representación del tiempo de ejecución respecto del tamaño de la matriz')
 plt.show()
-print('Teniendo en cuenta que el color rojo corresponde al método "a mano", mientras que el amarillo corresponde a numpy solve y el azul a multiplicar por la inversa, el método más ineficiente es multiplicar la inversa de la matriz, y el más eficiente es el hecho "a mano"')
+#print('Teniendo en cuenta que el color rojo corresponde al método "a mano", mientras que el amarillo corresponde a numpy solve y el azul a multiplicar por la inversa, el método más ineficiente es multiplicar la inversa de la matriz, y el más eficiente es el hecho "a mano"')
