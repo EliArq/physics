577d8e5ee99bf4c3e3c33b79a890d8a13363706d
EliArq <Eliana11_1999@hotmail.com>
2025-07-21 00:45:17 +0200

    Update Euler methods vector.py

--- computational/Euler methods vector.py
+++ computational/Euler methods vector.py
@@ -1,11 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Sat Sep 28 11:31:09 2024
-
-@author: Usr
-"""
 #Euler differential equation methods using vectors
-import scipy as sci
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -16,7 +10,6 @@ x0 = 1
 v0 = 0
 
 delta_t = (t[1]-t[0])
-print(delta_t)
 
 def Euler_forward(x0,v0,t,alpha,omega,delta_t):
     x = np.zeros(len(t))
@@ -40,15 +33,14 @@ def Trapezoidal(x0,v0,t,alpha,omega,delta_t):
     v = np.zeros(len(t))
     x = np.zeros(len(t))
     v[0], x[0] = v0, x0
-    delta_t = (t[-1]-t[0])/len(t)
     #hay que utilizar euler forward para predecir los valores v[i+1] y x[i+1]
     for m in range(0,len(t)-1):
         v_antes = v[m] + delta_t*(-omega**2*x[m] -alpha*v[m])
-        x[m+1] = x[m]+ (delta_t/2*(v[m]+v_antes))
+        x[m+1] = x[m]+ (delta_t/2)*(v[m]+v_antes)
         v[m+1] = v[m] + (delta_t/2)*(-omega**2*x[m+1]-alpha*v_antes-omega**2*x[m]-alpha*v[m])
     return x,v
 
-def Exacta(x0,v0,t,alpha,omega,delta_t):
+def Exacta(x0,v0,t,alpha,omega):
     Omega = np.sqrt(omega**2 - alpha**2 / 4)
     C1 = x0
     C2 = (v0 + alpha * x0 / 2) / Omega
@@ -57,9 +49,15 @@ def Exacta(x0,v0,t,alpha,omega,delta_t):
 x1,v1= Euler_forward(x0,v0,t,alpha, omega,delta_t)
 x2,v2 = Euler_backward(x0,v0,t,alpha, omega,delta_t)
 x3,v3 = Trapezoidal(x0,v0, t,alpha, omega,delta_t)
-plt.plot(t,x1)
-plt.plot(t,x2)
-plt.plot(t,x3)
+exacta = Exacta(x0,v0,t,alpha,omega)
+plt.xlabel('t (s)')
+plt.ylabel('x(t)')
+plt.title('Soluciones de la ecuación diferencial usando métodos de Euler')
+plt.plot(t,x1, label = 'Euler forward')
+plt.plot(t,x2, label = 'Euler backward')
+plt.plot(t,x3, label = 'Trapezoidal')
+plt.plot(t,exacta, label = 'Exacta')
+plt.legend(loc = 'best')
 plt.show()
     
 
